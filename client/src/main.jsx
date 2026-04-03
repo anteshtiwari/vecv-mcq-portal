01da7ab88844ace8c0bfc83e04867aea20d163bc
Antesh Tiwari <anteshtiwari@Anteshs-MacBook-Air.local>
2026-04-03 06:12:53 +0530

    Connected React to live backend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom' // <-- This must be here
+import axios from 'axios' // <-- 1. Import axios
 
+// <-- 2. Set the base URL to your live backend
+axios.defaults.baseURL = 'https://vecv-mcq-portal.onrender.com';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
